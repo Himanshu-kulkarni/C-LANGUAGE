a227182c01ef23f969cf8878802c900e256b35f3
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-08-10 10:26:45 +0530

    writting code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    // this is my first C program
+
+    /*this my first big comment
+    it spans into multiple lines
+    and it is very big
+    no very big
+    */ 
+
+    printf("I like pizza\n");
+    printf("its really good\n");
+    printf("pizza are great to eat if you are not the one paying\n");
+    return 0;
+}
