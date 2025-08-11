4c9946bd15326bc1b029d15fdea3186e08a0732e
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-08-11 07:06:59 +0530

    variables

---
+++ main1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // variable : A reusable container for a value
+    //            behaves as if it were the value it contains
+
+    int age = 17;
+
+    printf("you are %d years old\n", age);
+
+    return 0; 
+
+}
