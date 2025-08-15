3843c3a1433572152a0ec4f9164d7f58e825a773
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-08-15 16:24:45 +0530

    added coments

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include<math.h>
 
-int main() {
+// # here means the preprocessor directive
+// it is used to include header files
+// header files contain the function prototypes and macros
+// that are used in the program
+// before the compilation of the program, the preprocessor
+// replaces the #include directive with the contents of the header file
+// it even removes the comments  from the header files
+int main() 
 
-    // this is my first C program
+/*
+int main() is the entry point of the program
+it is the first function that is executed when the program is run
+it returns an integer value to the operating system
+0 means the program has executed successfully
+non-zero means the program has failed to execute
+*/
+
+{
+
+    // this is my first C cooment
 
     /*this my first big comment
     it spans into multiple lines
@@ -13,6 +30,7 @@ int main() {
     printf("I like pizza\n");
     printf("its really good\n");
     printf("pizza are great to eat if you are not the one paying\n");
+    printf("I like to eat pizza with my friends\n");
     return 0;
 /*
 this is my second comment
