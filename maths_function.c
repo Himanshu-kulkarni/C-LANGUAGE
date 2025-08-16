be920c1afc84ad4059172a8f9778bb9512bc27ab
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-08-16 16:16:22 +0530

    maths

---
+++ maths_function.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float x = 4.44;
+
+    //x = sqrt(x);
+    //x = pow(x, 5);
+    //x = round(x);
+    //x = ceil(x);
+    //x = floor(x);
+    //x = abs(x);
+    
+
+    printf("%f", x);
+
+    return 0;
+}
