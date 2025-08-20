485795f5d736157e4d0c0231fb9e0a3739b7526f
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-08-20 22:48:55 +0530

    made them

--- nested_if_statement.c
+++ nested_if_statement.c
@@ -4,12 +4,30 @@
 int main()
 {
     float prize = 10.00;
-    bool isstudent = false;
+    bool isstudent = false; // 10% discount
+    bool issenior = true; // 20% discount
+
+    //student = $9
+    //senior =$8
+    //student+senior = $7
 
     if(isstudent){
-        printf("YOU GET A STUDENT DISCOUNT OF 10%\n");
-        prize = prize *= 0.9;
+        if(issenior){
+            printf("you get a student dicount of 10%\n");
+            printf("you get a senior dicount of 20%\n");
+            prize = prize*0.7;
+        }
+        else{
+            printf("you get a student discount of 10%");
+            prize = prize*0.9;
+        }
+    }
+    else{
+        printf("you get a senior dicount of 20%\n");
+        prize = prize*0.8;
     }
     
     printf("The prize of a ticket is: $%.2f\n", prize);
+
+    return 0;
 }
