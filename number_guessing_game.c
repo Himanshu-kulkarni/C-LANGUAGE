0ba2defeb0f9a39b44646f65e1662505c9910957
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-09-13 22:38:57 +0530

    just made

---
+++ number_guessing_game.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+    // Number guesing game
+
+    srand(time(NULL));
+
+
+    int guess = 0;
+    int tries = 0;
+    int min = 1;
+    int max = 100;
+    int answer = rand() % (max - min + 1) + 1;
+
+    printf("$$$ Number Guessing Game $$$\n");
+
+    do{
+        printf("Enter a number from 1 - 100: ");
+        scanf("%d", &guess);
+        tries++;
+
+        if(guess < answer)
+        {
+            printf("too low\n");
+        }
+        else if(guess > answer)
+        {
+            printf("Too High\n");
+        }
+    }while(guess != answer);
+
+    printf("\nHurry you got it right\nthe answer %d is correct\n", answer);
+    printf("it took you only %d tries to get it right\n", tries);
+
+
+return 0;
+}
